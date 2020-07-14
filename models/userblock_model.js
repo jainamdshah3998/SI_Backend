4c1c3b436fec4ab78210464eec2215d7e27f78a7
darshanmehta88987 <darshanmehta88987@gmail.com>
2020-07-15 03:43:26 +0530

    user added

--- models/userblock_model.js
+++ models/userblock_model.js
@@ -1,7 +1,7 @@
 var db=require('../dbconnec');
 var userblock={
     getAllUserBlock:function(callback){
-        return db.query('select * from userblock',callback);
+        return db.query('select userblock.*,users.* from userblock join users on users.userPhoneNumber = userblock.userPhoneNumber',callback);
     },
     addUserBlock:function(item,callback){
         return db.query("insert into userblock(userPhoneNumber,blockNumber,flatName,secretaryPhoneNumber) values(?,?,?,?)",[item.userPhoneNumber,item.blockNumber,item.flatName,item.secretaryPhoneNumber],callback);
@@ -10,7 +10,7 @@ var userblock={
         return db.query("delete from userblock where userPhoneNumber=? and blockNumber=? and flatName=? and secretaryPhoneNumber=?",[userPhoneNumber,blockNumber,flatName,secretaryPhoneNumber],callback);
     },
     getUserBlockById:function(blockNumber,flatName,secretaryPhoneNumber,callback){
-        return db.query("select * from userblock where blockNumber=? and flatName=? and secretaryPhoneNumber=?",[blockNumber,flatName,secretaryPhoneNumber],callback);
+        return db.query("select userblock.*,users.* from userblock join users on users.userPhoneNumber = userblock.userPhoneNumber join usertype on users.userTypeid = usertype.userTypeId where blockNumber=? and flatName=? and secretaryPhoneNumber=? and usertype.name = 'user'",[blockNumber,flatName,secretaryPhoneNumber],callback);
     },
     updateUserBlockById:function(blockNumber,flatName,secretaryPhoneNumber,item,callback){
         return db.query("update userblock set userPhoneNumber=? where blockNumber=? and flatName=? and secretaryPhoneNumber=?",[item.userPhoneNumber,blockNumber,flatName,secretaryPhoneNumber],callback);
